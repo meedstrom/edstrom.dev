badfa0109543c7d8cc2a2d593212d210c746981d
Martin Edström <meedstrom@teknik.io>
2023-01-28 21:11:19 +0100

    runOnceHack

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
 import './App.css'
-import React, { Suspense, useState, useEffect } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React, { Suspense
+                , useState
+                , useEffect
+                , useRef } from 'react'
+import { BrowserRouter
+         , Routes
+         , Route
+         , useParams } from 'react-router-dom'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
-function BlogPost () {
+function BlogPost({ posts }) {
   let { slug } = useParams()
+
+  console.log(slug)
+  console.log(posts)
+  function firstPostBySlug(posts, slug) {
+    for (let i = 0; i < posts.length; i++) {
+      if (posts[i].slug === slug) {
+        return posts[i]
+      }
+    }
+  }
+
+  let post = firstPostBySlug(posts, slug)
+  let content = post.content
+
+  console.log(content)
+  return (
+    <ReactMarkdown remarkPlugins={[remarkGfm]}>
+      {content}
+    </ReactMarkdown>
+  )
+}
+
+function useFirstRender() {
+  const ref = useRef(true);
+  const firstRender = ref.current;
+  ref.current = false;
+  return firstRender;
 }
 
 function App() {
   const [posts, setPosts] = useState([])
-  const [page, setPage] = useState('/')
-  const runOnceHack // this never changes
+  const runOnceHack = true // this never changes
+  const firstRender = useFirstRender()
 
   // useEffect is stuff to run before every render (be aware that async stuff
   // won't finish before the first render)
   useEffect(() => {
     fetch('http://localhost:4040/allposts', {
-        headers: { Accept: 'application/json' },
+        headers: { Accept: 'application/json' } //,
     }).then(x => x.json())
       .then(x => Object.values(x))
       .then(x => setPosts(x))
   }, [runOnceHack])
 
+  if (!firstRender)
   return (
     <BrowserRouter>
       <div className="main-container">
@@ -34,18 +68,13 @@ function App() {
             </ReactMarkdown>
           )}
 
-          <Routes>
-            {posts.map(post => {
-              return <Route key={post.id}
-                            path={'/' + post.slug}
-                            content={post.content}
-                            children={(props) =>
-                              <ReactMarkdown>{props.content}</ReactMarkdown>
-                            } /> }
-                      )}
-          </Routes>
+        <Routes>
+          <Route path="/" element={<p>You're on the root page.</p>} />
+          <Route path="/posts/:slug" element={<BlogPost posts={posts} />} />
+        </Routes>
 
         </Suspense>
+
 
       </div>
     </BrowserRouter>
