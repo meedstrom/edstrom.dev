a9fe3b90161dd2ebd7d756bb261c2574f1e0fa00
Martin Edström <meedstrom@teknik.io>
2023-02-03 21:20:27 +0100

    Improvements

--- src/App.js
+++ src/App.js
@@ -7,47 +7,74 @@ import { BrowserRouter
          , Route
          , Link
          , useParams } from 'react-router-dom'
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
 import useCookie from 'react-use-cookie'
+import { Markup } from 'interweave'
+// import useTable from 'react-table'
 
 function BlogPost({ posts }) {
-  let { slug } = useParams()
-  let post = posts.find(x => x.slug === slug)
+  const slug = useParams()["*"]
+  const post = posts.find(x => x.slug === slug)
   if (typeof post === 'undefined') {
     console.log(`Slug '${slug}' did not match any post`)
   }
-  else if (post.type === 'md') {
-    return (
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>
-        {post.content}
-      </ReactMarkdown>
-    )
-  }
-  else if (post.type === 'html') {
-    let chunk = {__html: post.content}
-    return (
-      <div dangerouslySetInnerHTML={chunk} />
-    )
+  else {
+    return <Markup content={post.content} />
   }
 }
 
+function fontFromLength(charCount) {
+  let frac = Math.log(charCount) / Math.log(500000)
+  let size = 2 + frac * 20
+  if (size < 7) {
+    size = 7
+  }
+  return size
+}
+
 function RootPage({ posts }) {
+  // const columns = React.useMemo(
+  //   () => [
+  //     {
+  //       Header: 'Title'
+  //       accessor: 'title'
+  //     },
+  //     {
+  //       Header: 'Date'
+  //       accessor: 'date'
+  //     },
+  //   ]
+  // )
+  // const tableInstance = useTable({columns, posts})
+  //  const {
+   // getTableProps,
+   // getTableBodyProps,
+   // headerGroups,
+   // rows,
+   // prepareRow,
+  // } = tableInstance
   return (
     <div>
       <h1>Martin's notes</h1>
-      <ul>
-        {posts.map(post => { return (
-                   <li key={post.id}>
-                     <Link to={'/posts/' + post.slug}>
-                       {'/posts/' + post.slug}
-                     </Link>
-                   </li>
-        )})}
-      </ul>
+      <table>
+        {posts.map(post => {
+          const fontSize = fontFromLength(post.content.length)
+          let urlText = post.title
+          if (post.title === '') urlText = post.slug
+          return (
+            <tr key={post.id}>
+              <td>
+                <Link to={'/posts/' + post.slug} style={{fontSize: fontSize + 'pt'}}>
+                  {urlText}
+                </Link>
+              </td>
+              <td>{post.date}</td>
+            </tr>
+          )})}
+      </table>
     </div>
   )
 }
+
 
 function Dashboard () {
   return <h2>Dashboard</h2>
@@ -61,18 +88,17 @@ async function loginUser(creds) {
   }).then(data => data.json())
 }
 
-function Login ({ setUserToken }) {
+function Login ({ setAuthToken }) {
   const [username, setUsername] = useState()
   const [password, setPassword] = useState()
 
   const handleSubmit = async x => {
     x.preventDefault()
     const token = await loginUser({ username, password })
-    setUserToken(token, {
+    setAuthToken(token, {
         days: 7
       , sameSite: 'Strict'
       , secure: true
-      // , httpOnly: true
     })
   }
 
@@ -89,7 +115,7 @@ function Login ({ setUserToken }) {
         <input type="password" onChange={x => setPassword(x.target.value)} />
       </label>
       <div>
-        <button type="submit">Submit</button>
+        <button type="submit">I accept a cookie, log me in</button>
       </div>
     </form>
     </div>
@@ -98,29 +124,27 @@ function Login ({ setUserToken }) {
 
 function App() {
   const [posts, setPosts] = useState([])
-  const [userToken, setUserToken] = useCookie('token')
+  const [authToken, setAuthToken] = useCookie('token')
 
   if (posts.length === 0) {
     fetch('http://localhost:4040/allposts', {
-      headers: { Accept: 'application/json' } //,
+      headers: { Accept: 'application/json' }
     }).then(x => x.json())
       .then(x => Object.values(x))
+    // .then(x => React.useMemo(() => [x]))  // for react-table
       .then(x => setPosts(x))
   }
 
-  if (!userToken) {
-    return <Login setUserToken={setUserToken} />
+  if (!authToken) {
+    return <Login setAuthToken={setAuthToken} />
   }
   return (
       <div className="main-container">
-
         <Suspense fallback={<p>Attempting to load. If it takes long, it prolly broke.</p>} >
         <BrowserRouter>
         <Routes>
           <Route path="/" element={<RootPage posts={posts} />} />
-          {/* TODO: allow slashes to be part of slug.  Maybe here the path can
-            * be just /posts/* and then BlogPost figures it out. */}
-          <Route path="/posts/:slug" element={<BlogPost posts={posts} />} />
+          <Route path="/posts/*" element={<BlogPost posts={posts} />} />
           <Route path="/login" />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
